e433a6a907ed835cd6f14ff54a60edf2429ba0cc
Martin Klepsch <martinklepsch@googlemail.com>
2021-06-26 01:55:13 +0200

    add default text to /brightness command

--- blinky_bot.py
+++ blinky_bot.py
@@ -51,8 +51,11 @@ def help(update, context):
 
 def brightness(update, context):
     """Send a message when the command /brightness is issued."""
-    config.brightness.set(context.args[0])
-    update.message.reply_text(f"Brightness set to {context.args[0]}")
+    if context.args:
+        config.brightness.set(context.args[0])
+        update.message.reply_text(f"Brightness set to {context.args[0]}")
+    else:
+        update.message.reply_text(f"What brightness do you want dear? E.g. /brightness 0.4")
 
 def mood(update, context):
     """Send a message when the command /mood is issued."""
