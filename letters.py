813c001308ced52573b744927770d16c9fe895ec
Chris <ch.roeseler@gmail.com>
2022-06-12 11:46:29 +0200

    letter to dotmatrix

---
+++ letters.py
@@ -0,0 +1,18 @@
+import sys
+from PIL import Image
+
+
+def dotting(path):
+    img = Image.open(path)
+    dots = img.convert('L')
+    letter_matrix = {'dots': []}
+    for x in range(20):
+        for y in range(15):
+            if (g_scale_value := dots.getpixel((x,y))) == 0:
+                letter_matrix['dots'].append((x,y))
+
+    letter_matrix['size'] = dots.size
+    return letter_matrix
+
+if __name__ == '__main__':
+    print(dotting(sys.argv[1]))
