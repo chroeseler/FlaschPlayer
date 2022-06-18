49ffcc184da03ae01f8fdf6984b3c14bb58b70e2
Chris <ch.roeseler@gmail.com>
2022-06-18 11:29:30 +0200

    refactor: moving text processing into text queue

--- text_queue.py
+++ text_queue.py
@@ -1,21 +1,24 @@
 from filelock import Timeout, FileLock
+from pathlib import Path
 import logging
 import config
 import numpy as np
-import letters
+import sys
+from PIL import Image
 import json
 
 queue_txt = f"{config.work_dir}/text_queue.txt"
 lock = FileLock(f"{queue_txt}.lock")
 
 logger = logging.getLogger(__name__)
+LETTERS = Path(f'{config.work_dir}/letter')
 
 def setup():
     open(queue_txt,"a").write("")
 
 def put(text):
     def add_char_coord(char, width, text):
-        if (char_dict := letters.get_coords(char)):
+        if (char_dict := get_coords(char)):
             for coords in char_dict['dots']:
                 text.append([coords[0]+width, coords[1]])
             width += char_dict['size'][0]
@@ -47,3 +50,21 @@ def pop():
             f.truncate() # set the file size to the current size
             return json.loads(firstLine)
 
+def dotting(path):
+    img = Image.open(path)
+    dots = img.convert('L')
+    letter_matrix = {'dots': []}
+    for x in range(20):
+        for y in range(15):
+            if (g_scale_value := dots.getpixel((x,y))) == 0:
+                letter_matrix['dots'].append((x,y))
+
+    letter_matrix['size'] = dots.size
+    return letter_matrix
+
+def get_coords(char):
+    try:
+        return dotting(LETTERS.joinpath(str(ord(char))+'.png'))
+    except FileNotFoundError:
+        logger.info('Letter not found')
+        return None
