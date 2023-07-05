7906c6969a46b2debcc76b8c31b9d85fa297e5f5
Chris <ch.roeseler@gmail.com>
2023-07-05 23:18:25 +0200

    feat: The Flaschplayer interface on a local webserver

---
+++ blinky_interface.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+import text_queue as txt
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        print(request.form['text'])
+        txt.put(request.form['text'])
+        return render_template('wait.html', text=request.form['text'], length=5 + len(request.form['text']))
+    return render_template('landing.html')
+
+
+@app.route('/wait/')
+def waiting():
+    return render_template('landing.html')
+
+
+if __name__ == '__main__':
+    txt.setup()
+    app.run(host='0.0.0.0', port=5000)
